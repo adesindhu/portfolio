f4243c269216c9970548af6734d6ac27626cf2f1
adesindhu <162825616+adesindhu@users.noreply.github.com>
2024-09-14 17:39:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var typed = new Typed("#element", {
+    strings: [
+        "C Programming",
+        "Java Programming",
+        "HTML And CSS",
+        "Web Devloper...",
+    ],
+    typeSpeed: 60,
+});
+
